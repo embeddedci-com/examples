0374c59a4b92ef7b6df38d14713ef82ab67451e6
Edward Viaene <ward@in4it.io>
2026-03-23 14:09:43 -0500

    Makefile changes and hal config

---
+++ selftest-stm32/stm32f4xx_hal_conf.h
@@ -0,0 +1,70 @@
+#ifndef __STM32F4xx_HAL_CONF_H
+#define __STM32F4xx_HAL_CONF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ########################## Module Selection ############################## */
+
+#define HAL_MODULE_ENABLED
+
+#define HAL_RCC_MODULE_ENABLED
+#define HAL_GPIO_MODULE_ENABLED
+#define HAL_UART_MODULE_ENABLED
+#define HAL_CORTEX_MODULE_ENABLED
+#define HAL_PWR_MODULE_ENABLED
+
+/* ########################## Oscillator Values ############################# */
+
+#ifndef HSI_VALUE
+#define HSI_VALUE    16000000U
+#endif
+
+#ifndef HSE_VALUE
+#define HSE_VALUE    8000000U
+#endif
+
+#ifndef LSI_VALUE
+#define LSI_VALUE    32000U
+#endif
+
+#ifndef LSE_VALUE
+#define LSE_VALUE    32768U
+#endif
+
+/* ########################## System Configuration ########################## */
+
+#define  VDD_VALUE                    3300U
+#define  TICK_INT_PRIORITY           0x0FU
+#define  USE_RTOS                    0U
+#define  PREFETCH_ENABLE             1U
+#define  INSTRUCTION_CACHE_ENABLE    1U
+#define  DATA_CACHE_ENABLE           1U
+
+/* ########################## Assert Selection ############################## */
+
+/* Uncomment if you want assert_param enabled */
+/* #define USE_FULL_ASSERT 1 */
+
+/* ########################## Includes ###################################### */
+
+#include "stm32f4xx_hal_rcc.h"
+#include "stm32f4xx_hal_gpio.h"
+#include "stm32f4xx_hal_uart.h"
+#include "stm32f4xx_hal_cortex.h"
+#include "stm32f4xx_hal_pwr.h"
+
+/* ########################## Assert ######################################## */
+
+#ifdef  USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line);
+#else
+#define assert_param(expr) ((void)0U)
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STM32F4xx_HAL_CONF_H */
